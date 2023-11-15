e67da9b0f879f2210cf5f48c41796f65953ecf38
Alberto <alberto.renteriac@iteso.mx>
2023-11-14 22:31:01 -0600

    Modal fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,5 @@
-
 import React from "react";
 import { ImCross } from "react-icons/im";
-import styled from "styled-components";
 import "./styles/Modal.css";
 
 export default function Modal({
@@ -9,87 +7,62 @@ export default function Modal({
   modalState,
   changeModalState,
   isMobile,
-  width = '46%',
-  title = 'Modal',
+  setIsMobile,
+  width = "46%",
+  title = "Modal",
 }) {
-  // TODO: Manejar cuando es un dispositivo movil
+  // TODO: Manejar cuando es un dispositivo móvil
+
+  const overlay = `
+    w-full h-full fixed top-0 left-0 bg-black bg-opacity-50 pt-10 pb-10
+    flex items-center justify-center z-10
+  `;
+
+  const modalContainer = `
+    h-[97%] bg-white relative rounded-md shadow-md
+    flex flex-col overflow-hidden
+  `;
+
+  const closeButton = `
+    absolute top-4 right-4 w-4 h-4 border-none bg-none cursor-pointer
+    transition duration-300 ease-in-out rounded-md text-gray-700
+    hover:bg-gray-200
+  `;
+
+  const modalContent = `mx-auto flex-1 overflow-auto`;
+
+  const modalHeader = `sticky top-0 bg-white flex items-center justify-center p-4`;
 
   return (
-
     <>
       {modalState && (
-        <Overlay>
-          <ModalContainer width={width}>
-            <ModalHeader>
-              <h3 className='font-medium text-5xl text-[#222222] text-center'>
+        <div className={overlay}>
+          <div
+            className={`${modalContainer} ${
+              isMobile ? "w-[95%]" : "w-[" + width + "]"
+            }`}
+          >
+            <div
+              className={`${modalHeader} ${isMobile ? "w-[95%]" : "w-[66%]"}`}
+            >
+              <h3 className="font-medium text-5xl text-[#222222] text-center">
                 {title}
               </h3>
-            </ModalHeader>
-            <CloseButton onClick={() => changeModalState(false)}>
-              <ImCross className='hover:bg-gray-200' />
-            </CloseButton>
-            <ModalContent>{children}</ModalContent>
-          </ModalContainer>
-        </Overlay>
+            </div>
+            <div
+              className={closeButton}
+              onClick={() => changeModalState(false)}
+            >
+              <ImCross className="hover:bg-gray-200" />
+            </div>
+            <div
+              className={`${modalContent} ${isMobile ? "w-[95%]" : "w-[66%]"}`}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
 }
-
-const Overlay = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background: rgba(0, 0, 0, 0.5);
-  padding: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ModalContainer = styled.div`
-  width: ${props => props.width};
-  height: 97%;
-  background: #fff;
-  position: relative;
-  border-radius: 5px;
-  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden; /* Evitar el desplazamiento horizontal si hay contenido grande */
-`;
-
-const ModalHeader = styled.div`
-  position: sticky;
-  top: 0;
-  background: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 20px;
-`;
-
-const CloseButton = styled.div`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  width: 20px;
-  height: 20px;
-  border: none;
-  background: none;
-  cursor: pointer;
-  transition: 0.3s ease all;
-  border-radius: 5px;
-  color: #222222;
-`;
-
-const ModalContent = styled.div`
-  width: 66%;
-  margin: 0 auto;
-  box-sizing: border-box;
-  flex: 1;
-  overflow: auto;
-  scrollbar-gutter: stable;
-`;
