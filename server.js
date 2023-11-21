9ff8776112d502264f2034bc7a61031cd28fc541
nathannik123@gmail.com <nathannik123@gmail.com>
2023-11-21 01:22:33 -0600

    Archivo server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Todo el show de express
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+app.use(cors({
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
+
+//
