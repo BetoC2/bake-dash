49aad2010cf1cd33301f85c8ff0e0b8f513fe009
nathannik123@gmail.com <nathannik123@gmail.com>
2023-11-21 23:21:07 -0600

    barcodeScanner first version

--- api/app.js
+++ api/app.js
@@ -3,9 +3,11 @@ import morgan from "morgan";
 import cors from "cors";
 import authRoutes from "./routes/auth_routes.js";
 import userRoutes from "./routes/user_routes.js";
-//import productRoutes from "./routes/product_routes.js";
+import BarcodeScanner from "./barcodeScanner";
+import React from "react";
 
 const app = express();
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -17,6 +19,45 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
-//app.use("/products", productRoutes);
 
+// En la misma página, podríamos hacer aparacer un modal
+app.post("/barcode", async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    console.log("Código de barras recibido:", code);
+    res.status(200).json({ message: "Código de barras guardado con éxito" });
+  } catch (error) {
+    console.error("Error al guardar el código de barras en la base de datos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+const handleBarcodeRead = async (code) => {
+  try {
+    const response = await fetch("http://localhost:3000/barcode", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+    });
+
+    const data = await response.json();
+    console.log(data.message);
+  } catch (error) {
+    console.error("Error al enviar el código de barras al servidor:", error);
+  }
+};
+
+/*
+const App = () => {
+  return (
+    <div>
+      <h1>Barcode Scanner App</h1>
+      <BarcodeScanner onBarcodeRead={handleBarcodeRead} />
+    </div>
+  );
+};
+*/
 export default app;
--- api/controllers/barcodeScanner.js
+++ api/controllers/barcodeScanner.js
@@ -1,46 +1,40 @@
 // First version of barcodeScanner
-import mongoose from "mongoose";
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+import React, { useEffect } from "react";
+import Quagga from "quagga";
 
-const app = express();
-const PORT = 3000;
+const barcodeScanner = ({ onBarcodeRead }) => {
+  useEffect(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#barcode-scanner"),
+        },
+        decoder: {
+          readers: ["ean_reader"],
+        },
+      },
+      (err) => {
+        if (err) {
+          console.error("Error al inicializar Quagga:", err);
+          return;
+        }
+        Quagga.start();
+      }
+    );
 
-const mongoConnection = "mongodb+srv://admin:admin@myapp.mfg5ynr.mongodb.net/BakeDashDB";
+    Quagga.onDetected((data) => {
+      const code = data.codeResult.code;
+      onBarcodeRead(code);
+    });
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoConnection, { useNewUrlParser: true });
-    console.log("Database connected successfully");
-  } catch (err) {
-    console.log(err);
-  }
+    return () => {
+      Quagga.stop();
+    };
+  }, [onBarcodeRead]);
+
+  return <div id="barcode-scanner" />;
 };
 
-const barcodeSchema = new mongoose.Schema({
-  code: String,
-});
-
-const Barcode = mongoose.model("Barcode", barcodeSchema);
-
-app.use(bodyParser.json());
-
-app.post("/barcode", async (req, res) => {
-  const { code } = req.body;
-
-  try{
-    // Guardar el código en la base de datos
-    const newBarcode = new Barcode({ code });
-    await newBarcode.save();
-
-    res.status(200).json({ message: "Código de barras guardado con éxito" });
-  }catch (error) {
-    console.error("Error al guardar el código de barras en la base de datos:", error);
-    res.status(500).json({ error: "Error interno del servidor" });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
-});
+export default barcodeScanner;
