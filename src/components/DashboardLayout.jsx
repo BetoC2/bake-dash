befdc7daa24155c7fe0f41ddf35e648434fabb4c
Alberto <alberto.renteriac@iteso.mx>
2023-11-26 16:27:37 -0600

    Logout partial

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./styles/layout.scss";
 import { HiMenuAlt2, HiUser } from "react-icons/hi";
 import { RiDashboardFill, RiLogoutBoxLine } from "react-icons/ri";
 import { BsArrowRightSquareFill } from "react-icons/bs";
 import { BiBarcode } from "react-icons/bi";
 import { FaBell, FaMoneyBillWave } from "react-icons/fa";
-import brandLogo from "../assets/img/bmagallanes_logo.png"
-import userImg from "../assets/img/user.png"
+import brandLogo from "../assets/img/bmagallanes_logo.png";
+import userImg from "../assets/img/user.png";
 
 function DashboardLayout({ children, currentPage }) {
-
+  const navigate = useNavigate();
   //Responsive openning
   const [isMobile, setIsMobile] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,22 +32,21 @@ function DashboardLayout({ children, currentPage }) {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const mobileMenuClasses = `mobile-menu fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center transition-opacity duration-300 ${isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-    }`;
+  const mobileMenuClasses = `mobile-menu fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center transition-opacity duration-300 ${
+    isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+  }`;
 
   //DAHSBOARD MANAGEMENT: ALLLOWS MAINTINING DASHBOARD OPEN WHEN CHANGING VIEW
   // Openning state for Dashboard using sessionStorage
   const [isDashboardOpen, setIsDashboardOpen] = useState(() => {
-    const storedDashboardState = sessionStorage.getItem('dashboardState');
+    const storedDashboardState = sessionStorage.getItem("dashboardState");
     return storedDashboardState ? JSON.parse(storedDashboardState) : true;
   });
 
   useEffect(() => {
     // Update sessionStorage when isDashboardOpen changes
-    sessionStorage.setItem('dashboardState', JSON.stringify(isDashboardOpen));
+    sessionStorage.setItem("dashboardState", JSON.stringify(isDashboardOpen));
   }, [isDashboardOpen]);
-
-
 
   // Session Storage Call
   const sesion = JSON.parse(sessionStorage.getItem("sesion"));
@@ -57,19 +56,49 @@ function DashboardLayout({ children, currentPage }) {
 
   if (sesion && sesion.employment === "Admin") {
     Menus = [
-      { title: "Dashboard", href: "/", icon: <RiDashboardFill className={`text-3xl cursor-pointer duration-500`} /> },
-      { title: "Productos", href: "/products", icon: <BiBarcode className={`text-3xl cursor-pointer duration-500`} /> },
-      { title: "Usuarios", href: "/users", icon: <HiUser className={`text-3xl cursor-pointer duration-500`} /> },
-      { title: "Ventas", href: "/sales", icon: <FaMoneyBillWave className={`text-3xl cursor-pointer duration-500`} /> }
+      {
+        title: "Dashboard",
+        href: "/",
+        icon: (
+          <RiDashboardFill className={`text-3xl cursor-pointer duration-500`} />
+        ),
+      },
+      {
+        title: "Productos",
+        href: "/products",
+        icon: <BiBarcode className={`text-3xl cursor-pointer duration-500`} />,
+      },
+      {
+        title: "Usuarios",
+        href: "/users",
+        icon: <HiUser className={`text-3xl cursor-pointer duration-500`} />,
+      },
+      {
+        title: "Ventas",
+        href: "/sales",
+        icon: (
+          <FaMoneyBillWave className={`text-3xl cursor-pointer duration-500`} />
+        ),
+      },
     ];
   } else if (sesion && sesion.employment === "Vendedor") {
     Menus = [
-      { title: "Ventas", href: "/sales", icon: <FaMoneyBillWave className={`text-3xl cursor-pointer duration-500`} /> }
+      {
+        title: "Ventas",
+        href: "/sales",
+        icon: (
+          <FaMoneyBillWave className={`text-3xl cursor-pointer duration-500`} />
+        ),
+      },
     ];
   }
 
-  return (
+  const logout = () => {
+    sessionStorage.removeItem("sesion");
+    navigate("/login");
+  };
 
+  return (
     <div className="layout-content">
       {isMobile ? (
         <div>
@@ -81,45 +110,93 @@ function DashboardLayout({ children, currentPage }) {
             <HiMenuAlt2 className="text-3xl" />
           </button>
           <div className="flex gap-x-4 items-center justify-left pl-8">
-            <img src={brandLogo} alt="" className={`$ w-40 text-main-white cursor-pointer`} />
+            <img
+              src={brandLogo}
+              alt=""
+              className={`$ w-40 text-main-white cursor-pointer`}
+            />
           </div>
           <main className="">{children}</main>
-
         </div>
-
       ) : (
         //DESKOPT
         <div className="main-container flex overflow-hidden">
           <div className="flex justify-between items-center navbar fixed top-0 w-full bg-main-gray z-9">
-            <img src={brandLogo} alt="" className="w-40 text-main-white cursor-pointer ml-8" />
+            <img
+              src={brandLogo}
+              alt=""
+              className="w-40 text-main-white cursor-pointer ml-8"
+            />
             <div className="relative">
               <FaBell
                 className="text-3xl text-main-dark bg-gray-200 cursor-pointer mr-8"
-                style={{ padding: '8px', borderRadius: '20%' }}
+                style={{ padding: "8px", borderRadius: "20%" }}
               />
             </div>
           </div>
 
-          <div className={` ${isDashboardOpen ? 'w-72' : 'w-20'} mt-32 deskopt-navbar h-screen duration-300 p-5 pt-8 pb-8 pl-4 pr-4 rounded-3xl relative top-0 left-0 m-5 flex flex-col items-center`} style={{ height: 'calc(86vh - 45px)' }}>
+          <div
+            className={` ${
+              isDashboardOpen ? "w-72" : "w-20"
+            } mt-32 deskopt-navbar h-screen duration-300 p-5 pt-8 pb-8 pl-4 pr-4 rounded-3xl relative top-0 left-0 m-5 flex flex-col items-center`}
+            style={{ height: "calc(86vh - 45px)" }}
+          >
             <div className="flex gap-x-4 items-center justify-center">
-              <img src={userImg} alt="" className={`w-12 text-main-white cursor-pointer duration 500`} />
-              <h1 className={`${!isDashboardOpen && 'hidden'} text-main-dark origin-left duration-300 text-xl`}>{sesion.name}</h1>
+              <img
+                src={userImg}
+                alt=""
+                className={`w-12 text-main-white cursor-pointer duration 500`}
+              />
+              <h1
+                className={`${
+                  !isDashboardOpen && "hidden"
+                } text-main-dark origin-left duration-300 text-xl`}
+              >
+                {sesion.name}
+              </h1>
             </div>
-            <BsArrowRightSquareFill onClick={() => setIsDashboardOpen(!isDashboardOpen)} className={`${isDashboardOpen && "rotate-180"} text-4xl absolute cursor-pointer -right-3 top-9 w-7`} />
+            <BsArrowRightSquareFill
+              onClick={() => setIsDashboardOpen(!isDashboardOpen)}
+              className={`${
+                isDashboardOpen && "rotate-180"
+              } text-4xl absolute cursor-pointer -right-3 top-9 w-7`}
+            />
             <ul className="pt-6">
               {Menus.map((menu, index) => (
-                <Link key={index} to={menu.href} className={`text-sm flex items-center gap-x-4 cursor-pointer p-2 pl-8 pr-8 mb-3 sm:pl-4 sm:pr-4 hover:bg-main-dark hover:text-main-white rounded-md ${currentPage === menu.title ? 'bg-main-dark text-main-white' : ''}`}>
+                <Link
+                  key={index}
+                  to={menu.href}
+                  className={`text-sm flex items-center gap-x-4 cursor-pointer p-2 pl-8 pr-8 mb-3 sm:pl-4 sm:pr-4 hover:bg-main-dark hover:text-main-white rounded-md ${
+                    currentPage === menu.title
+                      ? "bg-main-dark text-main-white"
+                      : ""
+                  }`}
+                >
                   {menu.icon}
-                  <span className={`${!isDashboardOpen && 'hidden'} origin-left duration-200 text-2xl`} >{menu.title}</span>
+                  <span
+                    className={`${
+                      !isDashboardOpen && "hidden"
+                    } origin-left duration-200 text-2xl`}
+                  >
+                    {menu.title}
+                  </span>
                 </Link>
               ))}
             </ul>
             {/* LOGOUT */}
             <div className="mt-auto">
               {/* Usa este div para hacer el logout Alb */}
-              <div className="flex justify-end">
-                <RiLogoutBoxLine className={`text-3xl cursor-pointer duration-500`} />
-                <span className={`${!isDashboardOpen && 'hidden'} origin-left duration-200 text-2xl`} >Logout</span>
+              <div className="flex justify-end" onClick={logout}>
+                <RiLogoutBoxLine
+                  className={`text-3xl cursor-pointer duration-500`}
+                />
+                <span
+                  className={`${
+                    !isDashboardOpen && "hidden"
+                  } origin-left duration-200 text-2xl`}
+                >
+                  Logout
+                </span>
               </div>
             </div>
           </div>
@@ -128,15 +205,15 @@ function DashboardLayout({ children, currentPage }) {
             <main className="mt-24">{children}</main>
           </div>
         </div>
-
-
       )}
 
       {isMobile && (
         <div className={mobileMenuClasses}>
           <button
             type="button"
-            className={`menu-button absolute right-0 top-0 p-5 ${isMenuOpen ? 'text-white' : ''}`}
+            className={`menu-button absolute right-0 top-0 p-5 ${
+              isMenuOpen ? "text-white" : ""
+            }`}
             onClick={toggleMenu}
           >
             <HiMenuAlt2 className="text-3xl" />
